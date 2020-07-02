c930119b184a2bd3dd9fa358e1c78d70fbb21c7e
Jakob Krigovsky <jakob.krigovsky@kontist.com>
2020-07-02 14:55:02 +0200

    Download XML file

---
+++ scripts/data-download.js
@@ -0,0 +1,20 @@
+'use strict';
+const path = require('path');
+const got = require('got');
+const cheerio = require('cheerio');
+const download = require('download');
+
+const pageUrl =
+  'https://www.bzst.de/DE/Service/Behoerdenwegweiser/Finanzamtsuche/finanzamtsuche_node.html';
+const downloadDirectory = path.join(__dirname, '..', 'data');
+
+(async () => {
+  const response = await got(pageUrl);
+  const $ = cheerio.load(response.body);
+  const downloadLink = $('h3:contains("GemFA XML-Export Datei")')
+    .closest('a')
+    .prop('href');
+  const downloadUrl = new URL(downloadLink, pageUrl);
+
+  await download(downloadUrl, downloadDirectory, {extract: true});
+})();
