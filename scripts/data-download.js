3ce586af9c3b0bcc7a44d30aabe83a169e057637
Ady Shehadeh <ady.shehadeh@kontist.com>
2021-08-26 00:00:16 +0300

    Update GemFA XML download

--- scripts/data-download.js
+++ scripts/data-download.js
@@ -6,17 +6,18 @@ const del = require('del');
 const download = require('download');
 
 const pageUrl =
-  'https://www.bzst.de/DE/Service/Behoerdenwegweiser/Finanzamtsuche/finanzamtsuche_node.html';
+  'https://www.govdata.de/daten/-/details/suche-nach-zustandigem-finanzamt';
 const downloadDirectory = path.join(__dirname, '..', 'data');
 
 (async () => {
   const response = await got(pageUrl);
   const $ = cheerio.load(response.body);
-  const downloadLink = $('h3:contains("GemFA XML-Export Datei")')
-    .closest('a')
-    .prop('href');
+  const downloadLink = $('a:contains("gemfa_xml_export_datei")').prop('href');
   const downloadUrl = new URL(downloadLink, pageUrl);
 
-  await del('data/GemFA_Export_*.xml');
-  await download(downloadUrl, downloadDirectory, {extract: true});
+  await del('data/gemfa_xml_export_datei.xml');
+  await download(downloadUrl, downloadDirectory, {
+    extract: true,
+    filename: 'gemfa_xml_export_datei.xml'
+  });
 })();
