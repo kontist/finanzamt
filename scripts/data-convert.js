263cff0dc831ced2238509affce202be1f3c597e
Ady Shehadeh <ady.shehadeh@kontist.com>
2021-08-27 14:45:05 +0300

    Extract tel, fax and address

--- scripts/data-convert.js
+++ scripts/data-convert.js
@@ -5,33 +5,71 @@ const xml2js = require('xml2js');
 const _ = require('lodash');
 const writeJsonFile = require('write-json-file');
 
-(async () => {
-  const xmlPaths = await globby('data/GemFA_Export_*.xml');
+const getAddresse = (xmlAddresses, addressType) => {
+  const xmlAddress = _.find(
+    xmlAddresses,
+    (address) => address.$['xsi:type'] === addressType
+  );
 
-  if (xmlPaths.length === 0) {
-    throw new Error('No XML file found');
+  if (!xmlAddress) return undefined;
+
+  const address = {
+    type: addressType,
+    strasse: xmlAddress.$.Strasse,
+    hausNr: xmlAddress.$.HausNr,
+    plz: xmlAddress.$.PLZ,
+    ort: xmlAddress.$.Ort
+  };
+
+  return address;
+};
+
+const getAddresses = (xmlAddresses) => {
+  const addressesTypes = ['HausanschriftType'];
+  const adresses = _.map(addressesTypes, (addressType) =>
+    getAddresse(xmlAddresses, addressType)
+  );
+  return _.every(adresses, _.isEmpty) ? undefined : adresses;
+};
+
+const getContactValue = (kontaktListe, attributeName) => {
+  const contactNode = _.find(kontaktListe, ['$.Bezeichnung', attributeName]);
+  return contactNode ? contactNode.$.Inhalt : undefined;
+};
+
+const readFile = async (path) => {
+  const paths = await globby(path);
+
+  if (paths.length === 0) {
+    throw new Error(`'No file found'. file pattern: ${path}`);
   }
 
-  const xmlPath = xmlPaths[0];
-  const xmlFile = await fs.readFile(xmlPath);
+  const fileContent = await fs.readFile(paths[0]);
+  return fileContent;
+};
+
+const getFinanzamt = (finanzamt) => {
+  const attributes = finanzamt.$;
+  const kontaktListe = finanzamt.KontaktListe[0].Kontakt;
+  const adresseListe = finanzamt.AdresseListe[0].Adresse;
+
+  const finanzamtJson = {
+    buFaNr: attributes.BuFaNr,
+    name: attributes.Name,
+    tel: getContactValue(kontaktListe, 'Tel'),
+    fax: getContactValue(kontaktListe, 'Fax'),
+    mail: getContactValue(kontaktListe, 'Mail'),
+    url: getContactValue(kontaktListe, 'URL'),
+    addresses: getAddresses(adresseListe)
+  };
+
+  return finanzamtJson;
+};
+
+(async () => {
+  const xmlFile = await readFile('data/GemFA_Export_*.xml');
   const input = await xml2js.parseStringPromise(xmlFile);
-
   const finanzamtListe = input.GemfaExport.FinanzamtListe[0].Finanzamt;
-
-  const output = finanzamtListe.map((finanzamt) => {
-    const attributes = finanzamt.$;
-    const kontaktListe = finanzamt.KontaktListe[0].Kontakt;
-
-    const mail = _.find(kontaktListe, ['$.Bezeichnung', 'Mail'])?.$.Inhalt;
-    const url = _.find(kontaktListe, ['$.Bezeichnung', 'URL'])?.$.Inhalt;
-
-    return {
-      buFaNr: attributes.BuFaNr,
-      name: attributes.Name,
-      mail,
-      url
-    };
-  });
-
+  const output = _.map(finanzamtListe, getFinanzamt);
   await writeJsonFile('data/finanzaemter.json', output);
 })();
