a4cf3eecabad2ba3f5bf1dc85dec50890375c95a
Jakob Krigovsky <jakob@krigovsky.com>
2024-01-04 15:36:11 +0100

    Update test
    1131 no longer has a fax property.

--- test.js
+++ test.js
@@ -13,7 +13,6 @@ it('returns information about the Finanzamt Prenzlauer Berg', () => {
       ort: 'Berlin'
     },
     tel: '030 9024-28 0',
-    fax: '030 9024-28 900',
     mail: 'poststelle@fa-prenzlauer-berg.verwalt-berlin.de',
     url: 'http://www.berlin.de/sen/finanzen'
   });
